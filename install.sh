545eb43ec06088d3339d2c82aa01ecfcf5ad66be
Evan <riprock@users.noreply.github.com>
2019-07-23 15:24:48 -0400

    Starting back up working on this project again. Have to reinstall python becuase something is wrong with my venv and it seems like something with python broke

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 apt update
 apt full-upgrade
-apt install python
+apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools python3-venv nginx
 pip install flask
